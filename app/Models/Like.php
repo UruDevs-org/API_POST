c32ccd7373399dcb8baa3db46da47b6501b51e0e
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-06-14 13:02:38 -0300

    Created the model comment

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Like extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $table = "likes";
+
+    public function user(): BelongsTo {
+        return $this -> belongsTo(User::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
@@ -13,32 +14,36 @@ class Post extends Model
     use HasFactory;
     use SoftDeletes;
     protected $table = "posts";
-    public function author(): BelongsTo
-    {
+
+    public function author(): BelongsTo {
         return $this -> belongsTo(User::class);
     }
-    public function attachments(): HasMany
-    {
+
+    public function attachments(): HasMany {
         return $this -> hasMany(Attachment::class);
     }
-    public function comments(): HasMany
-    {
+
+    public function comments(): HasMany {
         return $this -> hasMany(Comment::class);
     }
-    public function likes(): HasMany
-    {
+
+    public function likes(): HasMany {
         return $this -> hasMany(Like::class);
     }
-    public function published_in_group(): BelongsTo
-    {
+
+    public function published_in_group(): BelongsTo {
         return $this -> belongsTo(Group::class);
     }
-    public function is_event(): HasOne
-    {
+
+    public function is_event(): HasOne {
         return $this -> hasOne(Event::class);
     }
-    public function is_comment(): HasOne
-    {
+
+    public function is_comment(): HasOne {
         return $this -> hasOne(Comment::class);
+    }
+
+    public function reports(): HasMany {
+        return $this -> hasMany(Report::class);
     }
 }
