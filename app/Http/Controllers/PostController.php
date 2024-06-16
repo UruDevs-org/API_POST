1b073cb620dcad99ca96fc6f638c7924d7d94c35
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-06-15 21:19:09 -0300

    Changed the query to be more performative

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,11 +19,8 @@ class PostController extends Controller
     function ListComments(Request $request, $id) {
         $page = $request -> has("page") ? $request -> get("page") : 1;
         $limit = 20;
-        $comments = Comment::where("replies_to", $id) -> skip(($page -1) * 20) -> take($limit) -> get();
-        $posts = [];
-        foreach($comments as &$comment) {
-            array_push($posts, Post::findOrFail($comment -> post));
-        }
+        $comments = Comment::where("replies_to", $id) -> skip(($page -1) * 20) -> take($limit) -> pluck("post") -> toArray();
+        $posts = Post::whereIn("id", $comments) -> get();
         return response() -> json($posts);
     }
 
